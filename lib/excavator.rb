c108a615690d3f3661f2e84c86898fe69c2b2d7b
Peter Bui <peter@paydrotalks.com>
2012-02-06 23:49:58 -0800

    Add TomDoc to Excavator module

--- lib/excavator.rb
+++ lib/excavator.rb
@@ -11,6 +11,74 @@ module Excavator
     end
   end
 
+  # Public: The current working directory for the Excavator script.
+  #
+  # Returns a Pathname.
+  def self.cwd
+    @cwd ||= Pathname.new(Dir.pwd).expand_path
+  end
+
+
+  # Public: The global Runner object. This object is called from
+  # Excavator.run to start the whole commandline parsing and command execution
+  # process.
+  #
+  # Returns a Runner class.
+  def self.runner
+    @runner ||= runner_class.new
+  end
+
+  # Public: The global Runner assignment method.
+  #
+  # runner - Any Class that implements Excavator::Runner's public api.
+  #
+  def self.runner=(runner)
+    @runner = runner
+  end
+
+  # Public: Start Excavator.
+  #
+  # On command error, this method will exit with a status of 1 and print
+  # the error message to $stderr.
+  #
+  # params - An Array of parameters. This is normally ARGV.
+  #
+  # Examples
+  #
+  #   Excavator.run(ARGV)
+  #   # => executes command passed in via ARGV
+  #
+  # Returns the object returned to by the command or exits with a status of 1.
+  def self.run(params)
+    begin
+      runner.run(params)
+
+    rescue => e
+      $stderr.puts e.message
+      if ENV['DEBUG'] == '1'
+        e.backtrace.each { |line| $stderr.puts line }
+      end
+
+      exit 1
+    end
+  end
+
+  # Internal: Setup class level configuration variables with defaults.
+  #
+  # Examples
+  #
+  #   module Excavator
+  #     config :test, "test"
+  #   end
+  #
+  #   Excavator.test
+  #   # => "test"
+  #
+  #   Excavator.test = "123"
+  #   Excavator.test
+  #   # => "123"
+  #
+  # Returns nothing.
   def self.config(name, default)
     @config   ||= {}
     @defaults ||= {}
@@ -26,36 +94,17 @@ module Excavator
     MOD
   end
 
-  def self.cwd
-    @cwd ||= Pathname.new(Dir.pwd).expand_path
-  end
-
+  # Internal: Resets the global Runner object. This is primarily used in
+  # testing.
+  #
+  # Examples
+  #
+  #   Excavator.reset!
+  #
+  # Returns nothing.
   def self.reset!
     self.runner = nil
   end
-
-  def self.runner
-    @runner ||= runner_class.new
-  end
-
-  def self.runner=(runner)
-    @runner = runner
-  end
-
-  def self.run(params)
-    begin
-      runner.run(params)
-
-    rescue => e
-      $stderr.puts e.message
-      if ENV['DEBUG'] == '1'
-        e.backtrace.each { |line| $stderr.puts line }
-      end
-
-      exit 1
-    end
-  end
-
 end
 
 require 'excavator/version'
@@ -69,7 +118,6 @@ require 'excavator/runner'
 require 'excavator/table_view'
 
 module Excavator
-  # Setup defaults classes
   config :command_paths,      []
   config :runner_class,       Runner
   config :namespace_class,    Namespace
